18ccea35a0e88d6dc48b3075023b1544e4f6611e
Ned Konz <ned@bike-nomad.com>
2019-10-31 09:16:15 -0700

    Added command line processing; set up for two input files

--- cpp/aubio_cpp.h
+++ cpp/aubio_cpp.h
@@ -53,9 +53,35 @@ class AubioProcessor {
 class AubioOnsetDetector : public AubioProcessor {
   typedef AubioProcessor super;
  public:
-  /** @param method one of
-   * energy, hfc, complexdomain, complex, phase, wphase,
-   * mkl, kl, specflux, specdiff, old_default
+  /** @param method (default="hfc")
+   * one of
+   energy -- Energy based distance
+   This function calculates the local energy of the input spectral frame.
+
+   hfc -- High-Frequency content
+     This method computes the High Frequency Content (HFC) of the input spectral frame.
+     The resulting function is efficient at detecting percussive onsets.
+
+   complexdomain -- Complex domain onset detection function
+    This function uses information both in frequency and in phase to determine changes in the spectral content that might correspond to musical onsets.
+    It is best suited for complex signals such as polyphonic recordings.
+   complex --
+
+   phase -- Phase based onset detection function
+This function uses information both in frequency and in phase to determine changes in the spectral content that might correspond to musical onsets.
+  It is best suited for complex signals such as polyphonic recordings.
+
+   wphase --
+
+   mkl --
+
+   kl --
+
+   specflux --
+
+   specdiff --
+
+   old_default
    */
   AubioOnsetDetector(char const *source_path,
                      uint_t samplerate = 0,
@@ -98,15 +124,20 @@ class AubioOnsetDetector : public AubioProcessor {
   /** answer the current delay in samples */
   uint_t delay() const { return aubio_onset_get_delay(o); }
  
+  /** threshold: 0.1 (more detections) to 1.0 (less); default=0.3 */
   void set_threshold(smpl_t threshold) {
     aubio_onset_set_threshold(o, threshold);
   }
   void set_delay(smpl_t delay) {
     aubio_onset_set_delay(o, delay);
   }
+  /** set minimum inter-onset interval
+   * @param minioi_ms (default=12)
+   */
   void set_minioi_ms(smpl_t minioi_ms) {
     aubio_onset_set_minioi_ms(o, minioi_ms);
   }
+  /** @param silence dB (default=-90) */
   void set_silence(smpl_t silence) {
     aubio_onset_set_silence(o, silence);
   }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,16 +6,123 @@
 #include "delay_line.h"
 #include <iostream>
 #include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+struct DetectorSettings {
+  uint_t hop_size;    // -h
+  smpl_t minioi_ms;   // -i
+  smpl_t silence;     // -s
+  smpl_t threshold;   // -t
+  char const *method;  // -m
+  char const *source_path;
+};
+
+char const *progname = nullptr;
+
+void usage() {
+  std::cerr
+      << "usage:\n"
+      << progname
+      << "[file1options] file1 [file2options] file2\noptions:\n"
+      << "\t[-h hopsize]    hop size (default=256)\n"
+      << "\t[-m method]     detection method (default|hfc|energy|complex|complexdomain|phase|wphase|mkl|kl|specflux|specdiff)\n"
+      << "\t[-i minioi_ms]  minimum inter-onset interval in ms (default=12)\n"
+      << "\t[-s silence]    silence threshold in dB (default=-90)\n"
+      << "\t[-t threshold]  detection threshold (0-1) (default=0.3)\n";
+}
+
+DetectorSettings const defaultSettings = { 256, 12.0, -90.0, 0.3, "default",
+    nullptr };
+
+bool getFloat(char const *arg, float &dest) {
+  char *str_end;
+  dest = std::strtof(arg, &str_end);
+  return *str_end == '\0';
+}
+
+bool getUint(char const *arg, uint_t &dest) {
+  char *str_end;
+  dest = static_cast<uint_t>(std::strtoul(arg, &str_end, 10));
+  return *str_end == '\0';
+}
+
+void printSettings(DetectorSettings const &settings) {
+  std::cerr << "settings: " << " -h " << settings.hop_size << " -i "
+            << settings.minioi_ms << " -s " << settings.silence << " -t "
+            << settings.threshold << " -m " << settings.method << " "
+            << settings.source_path << "\n";
+}
+
+bool getNextOnsetDetector(AubioOnsetDetector *&dest, int &lastarg, int argc,
+                          char const *argv[]) {
+  int i;
+  DetectorSettings settings = defaultSettings;
+  for (i = lastarg; i < argc; i++) {
+    if (!strcmp("-h", argv[i])) {
+      if (!getUint(argv[++i], settings.hop_size)) {
+        return false;
+      }
+      continue;
+    }
+    if (!strcmp("-i", argv[i])) {
+      if (!getFloat(argv[++i], settings.minioi_ms)) {
+        return false;
+      }
+      continue;
+    }
+    if (!strcmp("-s", argv[i])) {
+      if (!getFloat(argv[++i], settings.silence)) {
+        return false;
+      }
+      continue;
+    }
+    if (!strcmp("-t", argv[i])) {
+      if (!getFloat(argv[++i], settings.threshold)) {
+        return false;
+      }
+      continue;
+    }
+    if (!strcmp("-m", argv[i])) {
+      settings.method = argv[++i];
+      continue;
+    }
+    settings.source_path = argv[i];
+    break;
+  }
+  if (i >= argc || !settings.source_path) {
+    return false;
+  }
+  lastarg = i + 1;
+  dest = new AubioOnsetDetector(settings.source_path, 0, settings.hop_size,
+                                settings.method);
+  if (!dest)
+    return false;
+  dest->set_threshold(settings.threshold);
+  dest->set_minioi_ms(settings.minioi_ms);
+  dest->set_silence(settings.silence);
+  printSettings(settings);
+  return true;
+}
 
 int main(int argc, char const *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: " << argv[0] << " filename\n";
-    exit(1);
+  progname = argv[0];
+  AubioOnsetDetector *detectors[2];
+
+  int lastarg = 1;
+  for (int i = 0; i < 2; i++) {
+    if (!getNextOnsetDetector(detectors[i], lastarg, argc, argv)) {
+      usage();
+      exit(1);
+    }
   }
-  AubioOnsetDetector onsetDetector(argv[1], 0, 256);
-  while (onsetDetector.process_samples()) {
-    if (onsetDetector.is_onset()) {
-      printf("%.6f\n", onsetDetector.last_s());
+
+  while (detectors[0]->process_samples() && detectors[1]->process_samples()) {
+    if (detectors[0]->is_onset()) {
+      printf("1 %.6f\n", detectors[0]->last_s());
+    }
+    if (detectors[1]->is_onset()) {
+      printf("2 %.6f\n", detectors[1]->last_s());
     }
   }
 }
